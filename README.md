145514b4cc1c2749bde303ebdf397819d7c2c435
edoxy <gonnellino@gmail.com>
2021-03-21 12:57:41 +0100

    Readme file and introduction of the sparse function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MATLAB
 MATLAB scripts and function from lectures of Metodi Numerici PoliTO 2021
+
+## REQUIREMNTS
+
+To run this scripts via VSCode you need to install the python engine for your MATLAB installation; to know more follow the instruction on the MATWORKS website (find via the terminal extension)
--- gauss_seidel_sparse.m
+++ gauss_seidel_sparse.m
@@ -4,20 +4,23 @@ function [x, k, ier] = gauss_seidel(A, b, x, tol, kmax)
     % Syntax: [x, k, ier] = Gauss_Seidel(A, b, x, tol, kmax)
     %
     % Long funzione che applica l'algoritmo di gauss saidel a una matrice sparsa
+    %1:00 h nella lezione del 12
     n = length(b);
     d = diag(A);
     [c, r, a] = find(A' -diag(diag(A)));
     ier = 1
 
     for k = 1:kmax
-        y = x(1);
-        x(1) = (b(1) - A(1, 2:n) * x(2:n)) / A(1, 1);
-        xmax = abs(x(1));
-        emax = abs(x(1) - y);
 
-        for i = 2:n
-            x(i) = (b(i) - A(i, 1:i - 1) * x(1:i - 1) - ...
-                A(i, i + 1:n) * x(i + 1:n)) / A(i, i);
+        xmax = 0;
+        emax = 0;
+
+        for i = 1:n
+            y = x(i);
+            %x(i) = (b(i) - A(i, 1:i - 1) * x(1:i - 1) - A(i, i + 1:n) * x(i + 1:n)) / A(i, i);
+
+            s =
+            x(i) = (b(i) - s) / d(i);
 
             if abs(x(i)) > xmax
                 xmax = abs(x(i));
